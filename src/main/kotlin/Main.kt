4dc87b2a2846b4145d609e8fb14d9d89ef87d93f
josergdev <joserodriguezgarcia95@gmail.com>
2022-12-01 22:52:52 +0100

    Add files via upload

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+import days.day1part1
+import days.day1part2
+
+fun main(args: Array<String>) {
+    println(day1part1())
+    println(day1part2())
+}
---
+++ src/main/kotlin/days/day1.kt
@@ -0,0 +1,23 @@
+package days
+
+import java.nio.file.Files
+import java.nio.file.Paths
+import kotlin.streams.asSequence
+
+fun day1part1() = Files.lines(Paths.get("input/1.txt")).asSequence()
+    .fold(mutableListOf(mutableListOf<Int>())) { acc, line ->
+        if (line.isBlank()) acc.add(mutableListOf())
+        else acc.last().add(line.toInt())
+        acc
+    } .maxOf { it.sum() }
+
+fun day1part2() = Files.lines(Paths.get("input/1.txt")).asSequence()
+    .fold(mutableListOf(mutableListOf<Int>())) { acc, line ->
+        if (line.isBlank()) acc.add(mutableListOf())
+        else acc.last().add(line.toInt())
+        acc
+    } .map { it.sum()
+    } .asSequence()
+    .sortedDescending()
+    .take(3)
+    .sum()
