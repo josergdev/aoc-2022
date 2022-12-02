b384cd229d3f44537bdee128ef29c3cf22d0fcd7
josergdev <joserodriguezgarcia95@gmail.com>
2022-12-02 16:51:58 +0100

    day 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,12 @@
 import days.day1part1
 import days.day1part2
+import days.day2Part1
+import days.day2part2
 
-fun main(args: Array<String>) {
+fun main() {
     println(day1part1())
     println(day1part2())
+    
+    println(day2Part1())
+    println(day2part2())
 }
---
+++ src/main/kotlin/days/day2.kt
@@ -0,0 +1,74 @@
+package days
+
+import java.nio.file.Files
+import java.nio.file.Paths
+import kotlin.streams.asSequence
+
+sealed interface Option
+object Rock : Option
+object Paper : Option
+object Scissors : Option
+
+fun String.parseOption() = when (this) {
+    "A", "X" -> Rock
+    "B", "Y" -> Paper
+    "C", "Z" -> Scissors
+    else -> throw IllegalArgumentException(this)
+}
+
+fun Pair<Option, Option>.matchPoints() = when(this) {
+    Pair(Scissors, Rock), Pair(Rock, Paper), Pair(Paper, Scissors) -> 6
+    else -> when (this.first) {
+        this.second -> 3
+        else -> 0
+    }
+}
+
+fun Option.selectionPoints() = when(this) {
+    Rock -> 1
+    Paper -> 2
+    Scissors -> 3
+}
+
+fun day2Part1() = Files.lines(Paths.get("input/2.txt")).asSequence()
+    .map { it.split(" ") }
+    .map { Pair(it[0].parseOption(), it[1].parseOption()) }
+    .map { it.matchPoints() + it.second.selectionPoints() }
+    .sum()
+
+sealed interface Result
+object Win: Result
+object Draw: Result
+object Lose: Result
+
+fun String.parseResult() = when (this) {
+    "X" -> Lose
+    "Y" -> Draw
+    "Z" -> Win
+    else -> throw IllegalArgumentException(this)
+}
+
+fun neededFor(result: Result, option: Option) = when(result) {
+    Lose -> option.strongAgainst()
+    Draw -> option
+    Win -> option.weakAgainst()
+}
+
+fun Option.weakAgainst() = when(this) {
+    Rock -> Paper
+    Paper -> Scissors
+    Scissors -> Rock
+}
+
+fun Option.strongAgainst() = when(this) {
+    Rock -> Scissors
+    Paper -> Rock
+    Scissors -> Paper
+}
+
+fun day2part2() = Files.lines(Paths.get("input/2.txt")).asSequence()
+    .map { it.split(" ") }
+    .map { Pair(it[0].parseOption(), it[1].parseResult()) }
+    .map { Pair(it.first, neededFor(it.second, it.first)) }
+    .map { it.matchPoints() + it.second.selectionPoints() }
+    .sum()
