56bca8fd80bcfa088520742e4b546798ef397b25
josergdev <joserodriguezgarcia95@gmail.com>
2022-12-03 13:11:04 +0100

    day 3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,13 @@
-import days.day1part1
-import days.day1part2
-import days.day2Part1
-import days.day2part2
+import days.*
 
 fun main() {
+    println("Day 1:")
     println(day1part1())
     println(day1part2())
-    
+    println("Day 2:")
     println(day2Part1())
     println(day2part2())
+    println("Day 3:")
+    println(day3part1())
+    println(day3part2())
 }
---
+++ src/main/kotlin/days/day3.kt
@@ -0,0 +1,21 @@
+package days
+
+import java.nio.file.Files
+import java.nio.file.Paths
+import kotlin.streams.asSequence
+
+fun day3part1() =  Files.lines(Paths.get("input/3.txt")).asSequence()
+    .map { it.toList() }
+    .map { it.chunked(it.size / 2) }
+    .map { it[0].intersect(it[1].toSet()).first() }
+    .let { Pair(it, (CharRange('a', 'z').zip(IntRange(1, 26)) + CharRange('A', 'Z').zip(IntRange(27, 52))).toMap()) }
+    .let { (items, prio) -> items.map { prio[it]!! } }
+    .sum()
+
+fun day3part2() = Files.lines(Paths.get("input/3.txt")).asSequence()
+    .map { it.toSet() }
+    .windowed(3, 3)
+    .map { it[0].intersect(it[1]).intersect(it[2]).first() }
+    .let { Pair(it, (CharRange('a', 'z').zip(IntRange(1, 26)) + CharRange('A', 'Z').zip(IntRange(27, 52))).toMap()) }
+    .let { (items, prio) -> items.map { prio[it]!! } }
+    .sum()
