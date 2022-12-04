23934652ce898d25b4bf59f4562d7b8642725ee0
josergdev <joserodriguezgarcia95@gmail.com>
2022-12-04 11:35:23 +0100

    simplify day 4

--- src/main/kotlin/days/day4.kt
+++ src/main/kotlin/days/day4.kt
@@ -4,17 +4,16 @@ import java.nio.file.Files
 import java.nio.file.Paths
 import kotlin.streams.asSequence
 
-fun <T : Comparable<T>> ClosedRange<T>.containsAll(elements: Iterable<T>) = elements.all { this.contains(it) }
+fun <T : Comparable<T>> ClosedRange<T>.containsAll(elements: Iterable<T>) = elements.all(this::contains)
 
 fun day4part1() = Files.lines(Paths.get("input/4.txt")).asSequence()
     .map { it.split(',', '-').map { s -> s.toInt() } }
     .filter { IntRange(it[0], it[1]).containsAll(setOf(it[2], it[3])) || IntRange(it[2], it[3]).containsAll(setOf(it[0], it[1])) }
     .count()
 
-fun <T : Comparable<T>> ClosedRange<T>.containsSome(elements: Iterable<T>) = elements.any { this.contains(it) }
+fun <T : Comparable<T>> ClosedRange<T>.containsSome(elements: Iterable<T>) = elements.any(this::contains)
 
 fun day4part2() = Files.lines(Paths.get("input/4.txt")).asSequence()
     .map { it.split(',', '-').map { s -> s.toInt() } }
     .filter { IntRange(it[0], it[1]).containsSome(IntRange(it[2], it[3])) || IntRange(it[2], it[3]).containsSome(IntRange(it[0], it[1])) }
     .count()
-
