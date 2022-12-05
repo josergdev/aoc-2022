acf7be4e98dee73a64b803ee2d806a1aaeeb630e
josergdev <joserodriguezgarcia95@gmail.com>
2022-12-05 12:52:23 +0100

    change to immutable day 5

--- src/main/kotlin/days/day5.kt
+++ src/main/kotlin/days/day5.kt
@@ -7,24 +7,28 @@ import java.util.*
 fun parseCrates(crates: String) = crates.split("\n").dropLast(1)
     .map { it.windowed(3,4) }
     .map { it.map { s -> s.trim(' ', '[', ']') } }
-    .foldRight(mutableMapOf<Int, Stack<String>>()) { line, acc ->
-        line.forEachIndexed { index, crate ->
-            if (crate.isNotBlank())  
-                acc.getOrDefault(index + 1, Stack())
-                    .also { it.push(crate) }
-                    .also { acc[index + 1] = it }
+    .foldRight(mapOf<Int, Stack<String>>()) { line, cratesAcc ->
+        line.foldIndexed(cratesAcc) { index, acc, crate -> 
+            if (crate.isNotBlank()) 
+                acc.toMutableMap().let { map -> 
+                    map.getOrDefault(index + 1, Stack())
+                        .also { it.push(crate) }
+                        .also { map[index + 1] = it }
+                    map.toMap()
+                }
+            else acc
         }
-        acc
     }
 
 fun parseMoves(moves: String) = moves.split("\n")
     .map { it.replace("move ", "").replace(" from ", " ").replace(" to ", " ") }
     .map { it.split(" ").map(String::toInt) }
 
-fun MutableMap<Int, Stack<String>>.move(quantity: Int, from: Int, to: Int ): MutableMap<Int, Stack<String>> {
-    (1..quantity).forEach { _ -> this[to]!!.push(this[from]!!.pop()) }
-    return this
-}
+fun Map<Int, Stack<String>>.move(quantity: Int, from: Int, to: Int ) =
+    this.toMutableMap().let {  
+        (1..quantity).forEach { _ -> it[to]!!.push(it[from]!!.pop()) }
+        it.toMap()
+    }
 
 fun day5part1() = Files.readString(Paths.get("input/5.txt")).split("\n\n")
     .let { parseCrates(it[0]) to parseMoves(it[1]) }
@@ -32,12 +36,13 @@ fun day5part1() = Files.readString(Paths.get("input/5.txt")).split("\n\n")
     .entries.sortedBy { it.key }
     .joinToString("") { it.value.peek() }
 
-fun MutableMap<Int, Stack<String>>.move2(quantity: Int, from: Int, to: Int ): MutableMap<Int, Stack<String>> {
-    (1..quantity)
-        .fold(listOf<String>()) { acc, _ -> (acc + this[from]!!.pop()) }
-        .reversed().forEach { this[to]!!.push(it) }
-    return this
-}
+fun Map<Int, Stack<String>>.move2(quantity: Int, from: Int, to: Int ) = 
+    this.toMutableMap().let {
+        (1..quantity)
+            .fold(listOf<String>()) { acc, _ -> (acc + it[from]!!.pop()) }
+            .reversed().forEach { crate -> it[to]!!.push(crate) }
+        it.toMap()
+    }
 
 fun day5part2() = Files.readString(Paths.get("input/5.txt")).split("\n\n")
     .let { parseCrates(it[0]) to parseMoves(it[1]) }
