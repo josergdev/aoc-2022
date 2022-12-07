541e44a126b2827633bf74965ba631974516d030
josergdev <joserodriguezgarcia95@gmail.com>
2022-12-07 23:12:04 +0100

    simplify day 1

--- src/main/kotlin/days/day1.kt
+++ src/main/kotlin/days/day1.kt
@@ -2,21 +2,15 @@ package days
 
 import java.nio.file.Files
 import java.nio.file.Paths
-import kotlin.streams.asSequence
 
-fun day1part1() = Files.lines(Paths.get("input/1.txt")).asSequence()
-    .fold(mutableListOf(mutableListOf<Int>())) { acc, line ->
-        if (line.isBlank()) acc.add(mutableListOf())
-        else acc.last().add(line.toInt())
-        acc
-    } .maxOf { it.sum() }
+fun day1part1() = Files.readString(Paths.get("input/1.txt"))
+    .split("\n\n")
+    .map { it.split("\n").map { s -> s.toInt() } }
+    .maxOf { it.sum() }
 
-fun day1part2() = Files.lines(Paths.get("input/1.txt")).asSequence()
-    .fold(mutableListOf(mutableListOf<Int>())) { acc, line ->
-        if (line.isBlank()) acc.add(mutableListOf())
-        else acc.last().add(line.toInt())
-        acc
-    } 
+fun day1part2() = Files.readString(Paths.get("input/1.txt"))
+    .split("\n\n").asSequence()
+    .map { it.split("\n").map { s -> s.toInt() } }
     .map { it.sum() }
     .sortedDescending()
     .take(3)
