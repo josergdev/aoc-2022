e93d81281b538c6ae4b6cb4b114eef7f3877fcc9
josergdev <joserodriguezgarcia95@gmail.com>
2022-12-07 23:20:54 +0100

    simplify day 3

--- src/main/kotlin/days/day3.kt
+++ src/main/kotlin/days/day3.kt
@@ -7,14 +7,14 @@ import kotlin.streams.asSequence
 fun day3part1() =  Files.lines(Paths.get("input/3.txt")).asSequence()
     .map { it.chunked(it.length / 2) }
     .map { it[0].toSet().intersect(it[1].toSet()).first() }
-    .let { it to (CharRange('a', 'z') + CharRange('A', 'Z')).zip(IntRange(1, 52)).toMap() }
+    .let { it to (('a'..'z') + ('A'..'Z')).zip(1..52).toMap() }
     .let { (items, prio) -> items.map { prio[it]!! } }
     .sum()
 
 fun day3part2() = Files.lines(Paths.get("input/3.txt")).asSequence()
     .map { it.toSet() }
-    .windowed(3, 3)
+    .chunked(3)
     .map { it[0].intersect(it[1]).intersect(it[2]).first() }
-    .let { it to (CharRange('a', 'z') + CharRange('A', 'Z')).zip(IntRange(1, 52)).toMap() }
+    .let { it to (('a'..'z') + ('A'..'Z')).zip(1..52).toMap() }
     .let { (items, prio) -> items.map { prio[it]!! } }
     .sum()
